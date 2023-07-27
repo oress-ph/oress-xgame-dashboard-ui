8c6f1b9a214aed20bf9da4f9d488497b3e575f62
Oliver Enciso <oliverenciso07@gmail.com>
2023-07-27 22:33:44 +0800

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'xgames-marketplace-ui';
+  title = 'xgame-dashboard-ui';
 }
