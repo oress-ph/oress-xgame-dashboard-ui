d57b84326cc0815ca941f6e30a22ac4d89ea93dc
ernesr456 <50233898+ernesr456@users.noreply.github.com>
2023-07-27 10:29:31 +0800

    Revert "routing setup , topbar and footer"
    This reverts commit 331535123b704a2427141d084d5f0171209601f6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppSettings } from './app-settings';
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [AppSettings],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
