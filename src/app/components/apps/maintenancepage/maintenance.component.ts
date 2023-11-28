1c474294eea7dd9438501ee6119acc35ff2c51f1
ernesr456 <50233898+ernesr456@users.noreply.github.com>
2023-11-28 14:19:50 +0800

    fixing layout

--- src/app/components/apps/maintenancepage/maintenance.component.ts
+++ src/app/components/apps/maintenancepage/maintenance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'src/app/shared/services/layout.service';
 
 @Component({
   selector: 'app-maintenance',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintenanceComponent implements OnInit {
 
-  constructor() { }
+  constructor(public layout: LayoutService) { }
 
   ngOnInit(): void {
   }
