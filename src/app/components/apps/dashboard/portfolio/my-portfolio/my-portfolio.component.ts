e2070e0edf18e348cf98f7621cf2754ad21401b2
Joel Villena <villenajv43@gmail.com>
2024-03-12 19:23:48 +0800

    remove amount

--- src/app/components/apps/dashboard/portfolio/my-portfolio/my-portfolio.component.ts
+++ src/app/components/apps/dashboard/portfolio/my-portfolio/my-portfolio.component.ts
@@ -72,7 +72,8 @@ export class MyPortfolioComponent implements OnInit {
     const tokens = await this.portfolioService.setPortfolioDetails(currency, this.tokens, new_tokens);
     this.portfolioService.setTokens(tokens);
     this.selectedCurrency = currency;
-    this.totalBalance = this.portfolioService.getTotalBalance();
+    // this.totalBalance = this.portfolioService.getTotalBalance();
+    this.totalBalance = 0;
   }
 
   async ngOnInit(): Promise<void> {
